4e511263324680733170d386dd5184b37d3e9fb9
Matt Hillsdon <matt.hillsdon@microbit.org>
2021-09-06 16:44:47 +0100

    Neopixel happiness.

--- typeshed/stdlib/neopixel.pyi
+++ typeshed/stdlib/neopixel.pyi
@@ -1,99 +1,80 @@
 """
-The neopixel module lets you use Neopixel (WS2812) individually
-addressable RGB LED strips with the Microbit.
+The ``neopixel`` module lets you use NeoPixel (WS2812) individually addressable
+RGB and RGBW **V2** LED strips with the micro:bit. Note to use the ``neopixel`` module, you
+need to import it separately with::
 
-Note to use the neopixel module, you need to import it separately with:
+    import neopixel
 
-import neopixel
+.. note::
 
-Note
+    From our tests, the Microbit NeoPixel module can drive up to around 256
+    NeoPixels. Anything above that and you may experience weird bugs and
+    issues. The micro:bit can only supply 90mA **V1** or 190mA **V2**  to
+    external devices,larger numbers of NeoPixels require an external power
+    supply with common ground.
 
-From our tests, the Microbit Neopixel module can drive up to around 256
-Neopixels. Anything above that and you may experience weird bugs and issues.
+    NeoPixels are designed to work at 5V, but luckily they still function using
+    the 3V supply of the BBC micro:bit. Please note that the micro:bit edge
+    connector should not be connected to anything supplying 5V.
 
-NeoPixels are fun strips of multi-coloured programmable LEDs.
-This module contains everything to plug them into a micro:bit
-and create funky displays, art and games
+NeoPixels are fun strips of multi-coloured programmable LEDs. This module
+contains everything to plug them into a micro:bit and create funky displays,
+art and games.
 
+To connect a strip of neopixels you'll need to attach the micro:bit as shown
+below (assuming you want to drive the pixels from pin 0 - you can connect
+neopixels to pins 1 and 2 too). The label on the crocodile clip tells you where
+to attach the other end on the neopixel strip. The VDD pin may be labelled
+as something else on some variants of neopixels - for example "V+". In some
+cases it may be called "+5V" and it is only safe to use this if you have no
+other 5V devices connected.
 
+.. warning::
 
-Warning
+    Do not use the 3v connector on the micro:bit to power any more than 8
+    Neopixels at a time.
 
-Do not use the 3v connector on the Microbit to power any more than
-8 Neopixels at a time.
-
-If you wish to use more than 8 Neopixels, you must use a separate
-3v-5v power supply for the Neopixel power pin.
-
-
-Operations
-
-Writing the colour doesn’t update the display (use show() for that).
-
-np[0] = (255, 0, 128)  # first element
-np[-1] = (0, 255, 0)  # last element
-np.show()  # only now will the updated value be shown
-
-To read the colour of a specific pixel just reference it.
-
-print(np[0])
-
-Using Neopixels
-
-Interact with Neopixels as if they were a list of tuples.
-Each tuple represents the RGB (red, green and blue) mix of colours
-for a specific pixel. The RGB values can range between 0 to 255.
-
-For example, initialise a strip of 8 neopixels on a strip connected
-to pin0 like this:
-
-import neopixel
-np = neopixel.NeoPixel(pin0, 8)
-
-Set pixels by indexing them (like with a Python list). For instance,
-to set the first pixel to full brightness red, you would use:
-
-np[0] = (255, 0, 0)
-
-Or the final pixel to purple:
-
-np[-1] = (255, 0, 255)
-
-Get the current colour value of a pixel by indexing it. For example,
-to print the first pixel’s RGB value use:
-
-print(np[0])
-
-Finally, to push the new colour data to your Neopixel strip, use the .show() function:
-
-np.show()
-
-If nothing is happening, it’s probably because you’ve forgotten this final step..!
-
-Note
-
-If you’re not seeing anything change on your Neopixel strip,
-make sure you have show() at least somewhere otherwise your updates won’t be shown.
-
-
+    If you wish to use more than 8 Neopixels, you must use a separate 3v-5v
+    power supply for the Neopixel power pin.
 """
-from . microbit import MicroBitAnalogDigitalPin
+from microbit import MicroBitAnalogDigitalPin
+from typing import Tuple
 
-def NeoPixel(pin: MicroBitAnalogDigitalPin, n: int)-> None:
-    """
-    Initialise a new strip of n number of neopixel LEDs controlled via pin pin.
-    Each pixel is addressed by a position (starting from 0).
+class NeoPixel:
+    def __init__(self, pin: MicroBitAnalogDigitalPin, n: int, bpp: int = ...) -> None:
+        """
+        Initialise a new strip of ``n`` number of neopixel LEDs controlled via pin
+        ``pin``. The **V2** micro:bit can also support RGBW neopixels, so a third
+        argument can be passed to ``NeoPixel`` to indicate the number of bytes per
+        pixel (bpp). For RGBW, this is is ``4`` rather than the default of ``3`` for
+        RGB and GRB.
+        """
+        ...
+    def clear(self) -> None:
+        """
+        Clear all the pixels.
+        """
+        ...
+    def show(self) -> None:
+        """
+        Show the pixels. Must be called for any updates to become visible.
+        """
+        ...
+    def write(self) -> None:
+        """
+        **V2** Show the pixels. Must be called for any updates to become visible.
 
-    Neopixels are given RGB (red, green, blue) values between 0-255 as a tuple.
-    For example, (255,255,255) is white.
-    """
-
-def clear()->None:
-    """
-    Clear all the pixels.
-    """
-
-def show()->None:
-    """
-    Show the pixels. Must be called for any updates to become visible.
-    """
+        Equivalent to ``show``.
+        """
+        ...
+    def fill(self, colour: Tuple[int, ...]) -> None:
+        """
+        **V2** Colour all pixels a given RGB/RGBW value. The `colour` argument
+        should be a tuple of the same length as the mumber of bytes per pixel
+        (bpp). For example ``fill((0,0,255))``. Use in conjunction with
+        ``show()`` to update the Neopixels.
+        """
+        ...
+    def __setitem__(self, key: int, value: Tuple[int, ...]) -> None: ...
+    def __getitem__(self, key: int) -> Tuple[int, ...]: ...
+    def __len__(self) -> int: ...
