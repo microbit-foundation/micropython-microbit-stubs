22e7e2927188af848a0de7c4538d5eb1c670f176
Matt Hillsdon <matt.hillsdon@microbit.org>
2021-09-07 11:56:13 +0100

    GC

--- typeshed/stdlib/gc.pyi
+++ typeshed/stdlib/gc.pyi
@@ -1,64 +1,75 @@
-"""Garbage Collection
+"""Control the garbage collector"""
 
-This module provides control over the garbage collection process.
-"""
+from typing import overload
 
-def collect():
-    """
-    Run a garbage collection.
-    """
-    pass
 
-def disable():
-    """
-    Disable automatic garbage collection. Heap memory can still be 
-    allocated, and garbage collection can still be initiated 
-    manually using ``gc.collect()``.
-    """
-    pass
+def enable() -> None:
+    """Enable automatic garbage collection."""
+    ...
 
-def enable():
-    """
-    Enable automatic garbage collection.
-    """
-    pass
+def disable() -> None:
+   """Disable automatic garbage collection.  Heap memory can still be allocated,
+   and garbage collection can still be initiated manually using :meth:`gc.collect`."""
 
-def isenabled() -> bool:
-    """
-    Returns ``true`` when garbage collection is enabled.
-    """
-    pass
+def collect(): -> None:
+    """Run a garbage collection."""
+    ...
 
 def mem_alloc() -> int:
+    """Return the number of bytes of heap RAM that are allocated.
+
+    .. admonition:: Difference to CPython
+        :class: attention
+
+        This function is MicroPython extension.
     """
-    Return the number of bytes of heap RAM that are allocated.
-    """
-    pass
+    ...
 
 def mem_free() -> int:
-    """
-    Return the number of bytes of available heap RAM, or -1 if this 
-    amount is not known.
-    """
-    pass
+    """Return the number of bytes of available heap RAM, or -1 if this amount
+    is not known.
 
-def threshold(amount:int = None) -> int:
-    """
-    Set or query the additional GC allocation threshold. Normally, a 
-    collection is triggered only when a new allocation cannot be 
-    satisfied, i.e. on an out-of-memory (OOM) condition. If this 
-    function is called, in addition to OOM, a collection will be 
-    triggered each time after amount bytes have been allocated (in 
-    total, since the previous time such an amount of bytes have been 
-    allocated). amount is usually specified as less than the full 
-    heap size, with the intention to trigger a collection earlier 
-    than when the heap becomes exhausted, and in the hope that an 
-    early collection will prevent excessive memory fragmentation. 
-    This is a heuristic measure, the effect of which will vary from 
-    application to application, as well as the optimal value of the 
-    amount parameter.
+    .. admonition:: Difference to CPython
+        :class: attention
 
-    Calling the function without argument will return the current value 
-    of the threshold. A value of -1 means a disabled allocation threshold.
+        This function is MicroPython extension.
     """
-    pass
+    ...
+
+@overload
+def threshold() -> int:
+    """Query the additional GC allocation threshold.
+
+    .. admonition:: Difference to CPython
+        :class: attention
+
+        This function is a MicroPython extension. CPython has a similar
+        function - ``set_threshold()``, but due to different GC
+        implementations, its signature and semantics are different.
+    """
+    ...
+
+@overload
+def threshold(amount: int) -> None:
+    """Set the additional GC allocation threshold. Normally, a collection
+    is triggered only when a new allocation cannot be satisfied, i.e. on an
+    out-of-memory (OOM) condition. If this function is called, in addition to
+    OOM, a collection will be triggered each time after *amount* bytes have been
+    allocated (in total, since the previous time such an amount of bytes
+    have been allocated). *amount* is usually specified as less than the
+    full heap size, with the intention to trigger a collection earlier than when the
+    heap becomes exhausted, and in the hope that an early collection will prevent
+    excessive memory fragmentation. This is a heuristic measure, the effect
+    of which will vary from application to application, as well as
+    the optimal value of the *amount* parameter.
+
+    A value of -1 means a disabled allocation threshold.
+
+    .. admonition:: Difference to CPython
+        :class: attention
+
+        This function is a MicroPython extension. CPython has a similar
+        function - ``set_threshold()``, but due to different GC
+        implementations, its signature and semantics are different.
+    """
+    ...
