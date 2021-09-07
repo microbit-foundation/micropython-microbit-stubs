f041a58acf587100eb44fd21f81d607d915e79cf
Matt Hillsdon <matt.hillsdon@microbit.org>
2021-09-07 10:55:46 +0100

    Time.

--- typeshed/stdlib/utime.pyi
+++ typeshed/stdlib/utime.pyi
@@ -1,197 +1,115 @@
-# Copyright (c) 2019, Digi International, Inc.
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+"""
+The ``utime`` module provides functions for getting the current time and date, 
+measuring time intervals, and for delays.
+
+.. note::
+    The ``utime`` module is a MicroPython implementation of the standard Python 
+    ``time`` module. It can be imported using both ``import utime`` and 
+    ``import time``, but the module is the same.
+"""
 
 from typing import Optional, Union
 
-def gmtime(secs: Optional[int]=None) -> tuple:
-    """
-    Converts a time expressed in seconds since the Epoch into an 8-tuple which
-    contains:
-
-    * ``year``
-    * ``month``
-    * ``mday``
-    * ``hour``
-    * ``minute``
-    * ``second``
-    * ``weekday``
-    * ``yearday``
-
-    :param secs: The time to convert. If not provided or ``None``, then the
-        current time from the RTC is used.
-
-    :return: The tuple containing the converted time.
-    """
-    ...
-
-def localtime(secs: Optional[int]=None) -> tuple:
-    """
-    Converts a time expressed in seconds since the Epoch into an 8-tuple which
-    contains:
-
-    * ``year``
-    * ``month``
-    * ``mday``
-    * ``hour``
-    * ``minute``
-    * ``second``
-    * ``weekday``
-    * ``yearday``
-
-    :param secs: The time to convert. If not provided or ``None``, then the
-        current time from the RTC is used.
-
-    :return: The tuple containing the converted time.
-    """
-    ...
-
-def mktime(local_time: tuple) -> int:
-    """
-    This is inverse function of localtime. Its argument is a full 8-tuple
-    which expresses a time as per localtime. It returns an integer which is
-    the number of seconds since Jan 1, 2000.
-
-    :param local_time: Full 8-tuple which expresses a time as per localtime.
-
-    :return: Amount of seconds since Epoch for the given time.
-    """
-    ...
-
-def time() -> int:
-    """
-    Returns the number of seconds, as an integer, since the Epoch, assuming
-    that underlying RTC is set and maintained. If an RTC is not set, this
-    function returns number of seconds since a port-specific reference point
-    in time (for embedded boards without a battery-backed RTC, usually since
-    power up or reset).
-
-    If you want to develop portable MicroPython application, you should not
-    rely on this function to provide higher than second precision. If you need
-    higher precision, use ``ticks_ms()`` and ``ticks_us()`` functions, if you
-    need calendar time, ``localtime()`` without an argument is a better choice.
-
-    :return: Number of seconds, as an integer, since the Epoch.
-    """
-    ...
-
-def time_ns() -> int:
-    """
-    Returns the number of nanoseconds, as an integer, since the Epoch, assuming
-    that underlying RTC is set and maintained. If an RTC is not set, this
-    function returns number of seconds since a port-specific reference point
-    in time (for embedded boards without a battery-backed RTC, usually since
-    power up or reset).
-
-    If you want to develop portable MicroPython application, you should not
-    rely on this function to provide higher than second precision. If you need
-    higher precision, use ``ticks_ms()`` and ``ticks_us()`` functions, if you
-    need calendar time, ``localtime()`` without an argument is a better choice.
-
-    :return: Number of seconds, as an integer, since the Epoch.
-    """
-    ...
 
 def sleep(seconds: Union[int, float]) -> None:
     """
-    Sleeps for the given number of seconds. Some boards may accept seconds
-    as a floating-point number to sleep for a fractional number of seconds.
-    Note that other boards may not accept a floating-point argument, for
-    compatibility with them use ``sleep_ms()`` and ``sleep_us()`` functions.
-
-    :param seconds: Amount of time to sleep for.
+    Sleep for the given number of seconds. You can use a floating-point number 
+    to sleep for a fractional number of seconds, or use the 
+    :func:`utime.sleep_ms()` and :func:`utime.sleep_us()` functions.
     """
     ...
 
 def sleep_ms(ms: int) -> None:
     """
-    Delays for given number of milliseconds, should be positive or 0.
-
-    :param ms: Delay in milliseconds.
+    Delay for given number of milliseconds, should be positive or 0.
     """
     ...
 
 def sleep_us(us: int) -> None:
     """
-    Delays for given number of microseconds, should be positive or 0.
-
-    :param us: Delay in microseconds
+    Delay for given number of microseconds, should be positive or 0.
     """
     ...
 
 def ticks_ms() -> int:
     """
-    Returns the uptime of the XBee module in milliseconds.
-
-    :return: The uptime of the module in milliseconds.
+    Returns an increasing millisecond counter with an arbitrary reference point, 
+    that wraps around after some value.
     """
     ...
 
 def ticks_us() -> int:
     """
-    Returns the uptime of the XBee module in microseconds.
-
-    :return: The uptime of the module in microseconds.
-    """
-    ...
-
-def ticks_cpu() -> int:
-    """
-    Returns the uptime of the module in microseconds but with a higher
-    resolution (faster).
-
-    :return: The uptime of the module in microseconds.
+    Just like :func:`utime.ticks_ms()` above, but in microseconds.
     """
     ...
 
 def ticks_add(ticks: int, delta: int) -> int:
     """
-    Offsets ticks value by a given number, which can be either positive or
-    negative. Given a ticks value, this function allows to calculate ticks
-    value delta ticks before or after it, following modular-arithmetic
-    definition of tick values (see ``ticks_ms()``).
+    Offset ticks value by a given number, which can be either positive or 
+    negative. Given a ticks value, this function allows to calculate ticks 
+    value delta ticks before or after it, following modular-arithmetic 
+    definition of tick values.
 
-    This method is useful for calculating deadlines for events/tasks.
+    Example:
 
-    **Note**: You must use ``ticks_diff()`` function to work with deadlines.
+    .. code-block:: python
 
-    :param ticks: Number obtained from a direct result of call to
-        ``ticks_ms()``, ``ticks_us()``, or ``ticks_cpu()`` functions (or from
-        previous call to ``ticks_add()``)
-    :param delta: Arbitrary integer number or numeric expression.
+        # Find out what ticks value there was 100ms ago
+        print(ticks_add(time.ticks_ms(), -100))
 
-    :return: Returns the result of the add operation.
+        # Calculate deadline for operation and test for it
+        deadline = ticks_add(time.ticks_ms(), 200)
+        while ticks_diff(deadline, time.ticks_ms()) > 0:
+            do_a_little_of_something()
+
+        # Find out TICKS_MAX used by this port
+        print(ticks_add(0, -1))
     """
     ...
 
 def ticks_diff(ticks1: int, ticks2: int) -> int:
     """
-    Measures the period (ticks) difference between values returned from
-    ``ticks_ms()``, ``ticks_us()``, or ``ticks_cpu()`` functions, as a signed
-    value which may wrap around, so directly subtracting them is not supported.
+    Measure ticks difference between values returned from 
+    :func:`utime.ticks_ms()` or :func:`ticks_us()` functions, as a signed value
+    which may wrap around.
 
-    The argument order is the same as for subtraction operator,
+    The argument order is the same as for subtraction operator, 
     ``ticks_diff(ticks1, ticks2)`` has the same meaning as ``ticks1 - ticks2``.
 
-    :param ticks1: Ticks that precede in time the value of ``ticks2``.
-    :param ticks2: Second (newer) ticks value.
+    :func:`utime.ticks_diff()` is designed to accommodate various usage 
+    patterns, among them:
 
-    :return: The difference between the given ticks values.
+    Polling with timeout. In this case, the order of events is known, and you
+    will deal only with positive results of :func:`utime.ticks_diff()`:
+
+    .. code-block:: python
+
+        # Wait for GPIO pin to be asserted, but at most 500us
+        start = time.ticks_us()
+        while pin.value() == 0:
+            if time.ticks_diff(time.ticks_us(), start) > 500:
+                raise TimeoutError
+
+
+    Scheduling events. In this case, :func:`utime.ticks_diff()` result may be
+    negative if an event is overdue:
+
+
+    .. code-block:: python
+
+        # This code snippet is not optimized
+        now = time.ticks_ms()
+        scheduled_time = task.scheduled_time()
+        if ticks_diff(scheduled_time, now) > 0:
+            print("Too early, let's nap")
+            sleep_ms(ticks_diff(scheduled_time, now))
+            task.run()
+        elif ticks_diff(scheduled_time, now) == 0:
+            print("Right at time!")
+            task.run()
+        elif ticks_diff(scheduled_time, now) < 0:
+            print("Oops, running late, tell task to run faster!")
+            task.run(run_faster=true)
     """
     ...
