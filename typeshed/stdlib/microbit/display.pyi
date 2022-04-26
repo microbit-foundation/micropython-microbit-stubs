2edffa5c07e441333d49533f4fad41d6bc2ac4c7
Matt Hillsdon <matt.hillsdon@microbit.org>
2022-04-26 14:08:25 +0100

    Single quotes for consistency.

--- typeshed/stdlib/microbit/display.pyi
+++ typeshed/stdlib/microbit/display.pyi
@@ -66,7 +66,7 @@ def scroll(
 ) -> None:
     """Scrolls a number or text on the display.
 
-    Example: ``display.scroll("micro:bit")``
+    Example: ``display.scroll('micro:bit)``
 
     :param text: The string to scroll. If ``text`` is an integer or float it is first converted to a string using ``str()``.
     :param delay: The ``delay`` parameter controls how fast the text is scrolling.
