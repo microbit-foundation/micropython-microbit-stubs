1359e1a14b52d3832eb53939a55ca8aa0bffdae4
Matt Hillsdon <matt.hillsdon@microbit.org>
2022-06-08 18:43:36 +0100

    Fix packaging script.

--- scripts/browser-package.py
+++ scripts/browser-package.py
@@ -8,31 +8,28 @@ import os
 import json
 
 
-def create_typeshed_JSON(source_path, lang):
+def create_typeshed_json(source_path, lang):
     results = dict(files={})
-    for (source, prefix) in ((source_path, "/typeshed/"), ("config", "/src/")):
-        for (dirpath, dirnames, filenames) in os.walk(source):
+    output_path = f"typeshed.{lang}.json"
+    print(f"{source_path} -> {output_path}")
+    for (source_dir, prefix) in ((source_path, "/typeshed/"), ("config", "/src/")):
+        strip_parts = len(source_dir.split(os.path.sep))
+        for (dirpath, dirnames, filenames) in os.walk(source_dir):
             for f in sorted(filenames):
-                path = os.path.join(dirpath, f)
-                try:
-                    destination = os.path.join(
-                        prefix, os.path.join(*path.split(os.path.sep)[3:])
-                    )
-                except:
-                    # Correct destination for config file.
-                    destination = os.path.join(
-                        prefix, os.path.join(*path.split(os.path.sep)[1:])
-                    )
-                print(f"{path} -> {destination}")
-                text = open(path, "r", encoding="utf-8").read()
+                source_path = os.path.join(dirpath, f)
+                destination = os.path.join(
+                    prefix,
+                    os.path.join(*source_path.split(os.path.sep)[strip_parts:]),
+                )
+                print(f"  {source_path} -> {destination}")
+                text = open(source_path, "r", encoding="utf-8").read()
                 results["files"][destination] = text
 
-    with open(f"typeshed.{lang}.json", "w", encoding="utf-8") as f:
+    with open(output_path, "w", encoding="utf-8") as f:
         f.write(json.dumps(results, indent=2))
 
 
-for (dirpath, dirnames, filenames) in os.walk("lang"):
-    for dirname in dirnames:
-        source_path = os.path.join(dirpath, dirname, "typeshed")
-        create_typeshed_JSON(source_path, dirname)
-    break
+if __name__ == "__main__":
+    for lang in os.listdir("lang"):
+        source_path = os.path.join("lang", lang, "typeshed")
+        create_typeshed_json(source_path, lang)
