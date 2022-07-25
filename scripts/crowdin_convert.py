674a8628a357f2083806db3b33cba7a711ec0803
Robert Knight <robert@microbit.org>
2022-07-25 14:42:21 +0100

    Update crowdin script to use suffixed overloads.

--- scripts/crowdin_convert.py
+++ scripts/crowdin_convert.py
@@ -271,9 +271,14 @@ def update_docstrings(
             key_root = ".".join([*self.key, name])
             key = key_root
             suffix = 1
-            while key in self.used_keys:
-                key = f"{key_root}-{suffix}"
-                suffix += 1
+            if isinstance(node, ast.FunctionDef):  # ctx.id
+                for decorator in node.decorator_list:
+                    if hasattr(decorator, "id"):
+                        if decorator.id == "overload":
+                            key = f"{key}-{suffix}"
+                            while key in self.used_keys:
+                                suffix += 1
+                                key = f"{key_root}-{suffix}"
             self.used_keys.add(key)
             update_docstring(node, key, en_json, translated_json)
 
