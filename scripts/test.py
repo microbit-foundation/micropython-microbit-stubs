e62fd440d4218b3360317ddbdb5bb84366c83c94
Matt Hillsdon <matt.hillsdon@microbit.org>
2022-09-07 10:32:22 +0100

    Fix test to cope with en-only test cases.

--- scripts/test.py
+++ scripts/test.py
@@ -10,6 +10,15 @@ for language in languages:
     typeshed = f"./lang/{language}/typeshed/"
     print(f"Testing typeshed at {typeshed} for language {language}")
     sys.stdout.flush()
+    examples = [d for d in os.listdir("examples") if language == "en" or d != "en-only"]
+    example_paths = [os.path.join("examples", d) for d in examples]
     subprocess.check_call(
-        ["pyright", "-p", "test-pyrightconfig.json", "--typeshed-path", typeshed]
+        [
+            "pyright",
+            "-p",
+            "test-pyrightconfig.json",
+            "--typeshed-path",
+            typeshed,
+            *example_paths,
+        ]
     )
